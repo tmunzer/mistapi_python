0a92d9f8a349f1e07def4e2bbcc3bf019e8a0ed1
Thomas Munzer <tmunzer@juniper.net>
2026-09-15 08:35:32 -0700

    feat(orgs): add AOSCX and EdgeConnect register command APIs

--- src/mistapi/api/v1/orgs/edgeconnect.py
+++ src/mistapi/api/v1/orgs/edgeconnect.py
@@ -14,9 +14,11 @@ from mistapi import APISession as _APISession
 from mistapi.__api_response import APIResponse as _APIResponse
 
 
-def getOrgAosRegisterCmd(mist_session: _APISession, org_id: str) -> _APIResponse:
+def getOrgEdgeconnectRegisterCmd(
+    mist_session: _APISession, org_id: str
+) -> _APIResponse:
     """
-    API doc: https://www.juniper.net/documentation/us/en/software/mist/api/http/api/orgs/devices/aos/get-org-aos-register-cmd
+    API doc: https://www.juniper.net/documentation/us/en/software/mist/api/http/api/orgs/devices/edgeconnect/get-org-edgeconnect-register-cmd
 
     PARAMS
     -----------
@@ -33,7 +35,7 @@ def getOrgAosRegisterCmd(mist_session: _APISession, org_id: str) -> _APIResponse
         response from the API call
     """
 
-    uri = f"/api/v1/orgs/{org_id}/aos/register_cmd"
+    uri = f"/api/v1/orgs/{org_id}/edgeconnect/register_cmd"
     query_params: dict[str, str] = {}
     resp = mist_session.mist_get(uri=uri, query=query_params)
     return resp
