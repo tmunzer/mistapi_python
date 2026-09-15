9dc604d98536ced4877759770e3d86133a4914cb
Thomas Munzer <tmunzer@juniper.net>
2026-09-15 08:35:04 -0700

    feat(marvisclient): add Marvis client events definitions API endpoint

---
+++ src/mistapi/api/v1/const/marvisclient_events.py
@@ -0,0 +1,35 @@
+"""
+--------------------------------------------------------------------------------
+------------------------- Mist API Python CLI Session --------------------------
+
+    Written by: Thomas Munzer (tmunzer@juniper.net)
+    Github    : https://github.com/tmunzer/mistapi_python
+
+    This package is licensed under the MIT License.
+
+--------------------------------------------------------------------------------
+"""
+
+from mistapi import APISession as _APISession
+from mistapi.__api_response import APIResponse as _APIResponse
+
+
+def listMarvisClientEventsDefinitions(mist_session: _APISession) -> _APIResponse:
+    """
+    API doc: https://www.juniper.net/documentation/us/en/software/mist/api/http/api/constants/definitions/list-marvis-client-events-definitions
+
+    PARAMS
+    -----------
+    mistapi.APISession : mist_session
+        mistapi session including authentication and Mist host information
+
+    RETURN
+    -----------
+    mistapi.APIResponse
+        response from the API call
+    """
+
+    uri = "/api/v1/const/marvisclient_events"
+    query_params: dict[str, str] = {}
+    resp = mist_session.mist_get(uri=uri, query=query_params)
+    return resp
